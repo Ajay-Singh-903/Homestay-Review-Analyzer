4eedccd6843892fd20870b48cb157244a07dded0
Ajay Singh <negiajay903@gmail.com>
2026-06-22 21:03:25 +0530

    feat:added a message toast

---
+++ src/components/ui/Toast.jsx
@@ -0,0 +1,11 @@
+/**
+ * Toast Component
+ *
+ * Shows notification messages
+ */
+"use client";
+import { Toaster } from "react-hot-toast";
+
+export default function Toast() {
+  return <Toaster position="top-right" />;
+}
