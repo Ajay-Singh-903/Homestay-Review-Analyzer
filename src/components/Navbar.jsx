ba781ad1981f870932f78c142172ae6d4c4f6835
Ajay Singh <negiajay903@gmail.com>
2026-06-22 21:06:40 +0530

    feat:fixed Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,22 @@
+"use client";
+
 import Link from "next/link";
+import ThemeToggle from "./ThemeToggle";
 
 export default function Navbar() {
   return (
-    <nav className="bg-green-700 text-white px-6 py-4 flex justify-between">
-      <h1>Homestay Review Analyzer</h1>
+    <nav className="bg-green-700 dark:bg-gray-900 text-white px-6 py-4 flex justify-between items-center">
+      <h1 className="font-bold">
+        Homestay Review Analyzer
+      </h1>
 
-      <div className="flex gap-4">
+      <div className="flex flex-wrap items-center gap-4">
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
         <Link href="/dashboard">Dashboard</Link>
         <Link href="/login">Login</Link>
+
+        <ThemeToggle />
       </div>
     </nav>
   );
