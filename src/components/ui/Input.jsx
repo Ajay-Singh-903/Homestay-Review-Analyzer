10d70cf561b7a62c6164f9d35ee827bb208ba88a
Ajay Singh <negiajay903@gmail.com>
2026-06-22 20:57:50 +0530

    feat:added an input box

---
+++ src/components/ui/Input.jsx
@@ -0,0 +1,40 @@
+/**
+ * Input Component
+ *
+ * Props:
+ * label
+ * placeholder
+ * type
+ * value
+ * onChange
+ * error
+ */
+"use client";
+export default function Input({
+  label,
+  placeholder,
+  type = "text",
+  value,
+  onChange,
+  error,
+}) {
+  return (
+    <div className="mb-4">
+      <label className="block mb-1">{label}</label>
+
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className="border p-2 w-full rounded"
+      />
+
+      {error && (
+        <p className="text-red-500 text-sm mt-1">
+          {error}
+        </p>
+      )}
+    </div>
+  );
+}
