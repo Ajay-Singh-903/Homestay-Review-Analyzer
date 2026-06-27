f7d8a55770191fc9257ed5e0bb211c5626667782
Ajay Singh <negiajay903@gmail.com>
2026-06-27 11:58:56 +0530

    feat:dummy data stored

---
+++ backend/data/reviews.js
@@ -0,0 +1,31 @@
+const reviews = [
+  {
+    id: 1,
+    guestName: "Rahul Sharma",
+    review:
+      "The rooms were clean and the host was very friendly. Great experience!",
+    sentiment: "Positive",
+    theme: "Cleanliness",
+    response: "Thank you for your valuable feedback."
+  },
+  {
+    id: 2,
+    guestName: "Priya Singh",
+    review:
+      "Food was average but the location was beautiful.",
+    sentiment: "Neutral",
+    theme: "Food",
+    response: "Thank you. We will work on improving our food quality."
+  },
+  {
+    id: 3,
+    guestName: "John",
+    review:
+      "The room was dirty and service was very slow.",
+    sentiment: "Negative",
+    theme: "Cleanliness",
+    response: "We sincerely apologize for your experience."
+  }
+];
+
+module.exports = reviews;
